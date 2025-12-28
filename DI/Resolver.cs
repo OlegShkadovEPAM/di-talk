71c19d669acc67b9b34ccec82d21e58180d352a0
OlegShkadovEPAM <144143748+OlegShkadovEPAM@users.noreply.github.com>
2025-12-28 12:48:04 +0500

    Instantiate multiple parameters

--- DI/Resolver.cs
+++ DI/Resolver.cs
@@ -4,13 +4,17 @@ public class Resolver
 {
     public T Get<T>()
     {
-        // Get constructor parameter
+        // Get constructor parameters
         var constructor = typeof(T).GetConstructors().Single();
         var parameterInfo = constructor.GetParameters();
 
-        // Instantiate the parameter
-        var parameter = Activator.CreateInstance(parameterInfo[0].ParameterType);
-        var parameters = new object[]{parameter};
+        // Instantiate the parameters
+        var parameters = new object[parameterInfo.Length];
+        for (int i = 0; i < parameters.Length; i++)
+        {
+            parameters[i] = Activator.CreateInstance(
+                parameterInfo[i].ParameterType);
+        }
 
         return (T)Activator.CreateInstance(typeof(T), parameters);
     }
