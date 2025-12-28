72801225e09d93392dd5de0a66c2cc514713b30d
OlegShkadovEPAM <144143748+OlegShkadovEPAM@users.noreply.github.com>
2025-12-28 12:36:00 +0500

    Instantiate with one parameter constructor.

--- DI/Resolver.cs
+++ DI/Resolver.cs
@@ -1,10 +1,17 @@
-﻿
-namespace DI;
+﻿namespace DI;
 
 public class Resolver
 {
     public T Get<T>()
     {
-        return (T)Activator.CreateInstance(typeof(T));
+        // Get constructor parameter
+        var constructor = typeof(T).GetConstructors().Single();
+        var parameterInfo = constructor.GetParameters();
+
+        // Instantiate the parameter
+        var parameter = Activator.CreateInstance(parameterInfo[0].ParameterType);
+        var parameters = new object[]{parameter};
+
+        return (T)Activator.CreateInstance(typeof(T), parameters);
     }
 }
